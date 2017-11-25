9784ecdfe0a82a9118fb4089545e2debec10c13f
Tiago Araujo <taraujodesouza@gmail.com>
2017-11-25 15:30:48 -0200

    Adicionado cache de imagens offline

--- app/src/main/java/tgo1014/webgados/WebGadosApplication.kt
+++ app/src/main/java/tgo1014/webgados/WebGadosApplication.kt
@@ -2,7 +2,10 @@ package tgo1014.webgados
 
 import android.app.Application
 import android.arch.persistence.room.Room
+import com.squareup.picasso.OkHttpDownloader
+import com.squareup.picasso.Picasso
 import tgo1014.webgados.model.database.AdDatabase
+
 
 class WebGadosApplication : Application() {
 
@@ -11,5 +14,15 @@ class WebGadosApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         adDatabase = Room.databaseBuilder(this, AdDatabase::class.java, "webGadosAdDb").build()
+        offlinePicassoCache()
+    }
+
+    private fun offlinePicassoCache() {
+        val built = Picasso.Builder(this)
+                .downloader(OkHttpDownloader(this, Integer.MAX_VALUE.toLong()))
+                .build()
+//        built.setIndicatorsEnabled(true)
+//        built.isLoggingEnabled = true
+        Picasso.setSingletonInstance(built)
     }
 }
