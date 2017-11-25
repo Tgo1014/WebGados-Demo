9428a3378ea13b7ae3742412c3360cbf4a16c3a1
Tiago Araujo <taraujodesouza@gmail.com>
2017-11-25 15:46:14 -0200

    Adicionado métodos asincronos

--- app/src/main/java/tgo1014/webgados/model/MainModelImpl.kt
+++ app/src/main/java/tgo1014/webgados/model/MainModelImpl.kt
@@ -1,7 +1,6 @@
 package tgo1014.webgados.model
 
-import android.util.Log
-import kotlinx.coroutines.experimental.launch
+import kotlinx.coroutines.experimental.async
 import kotlinx.coroutines.experimental.runBlocking
 import retrofit2.Call
 import retrofit2.Callback
@@ -20,37 +19,39 @@ class MainModelImpl : MainContract.MainModel {
         this.database = database
     }
 
-    override fun getAll(): List<Ad> = database.adDao().getAll()
-
-    override fun getById(id: Int): Ad = database.adDao().getById(id)
-
-    override fun insert(ad: Ad) = database.adDao().insert(ad)
-
-    override fun deleteAd(ad: Ad) = database.adDao().deleteAd(ad)
-
-    private fun insertAll(adList: List<Ad>) = runBlocking<Unit> {
-        var job = launch { for (ad in adList) insert(ad) }
-        job.join()
+    override fun getAll(): List<Ad> = runBlocking {
+        return@runBlocking async { database.adDao().getAll() }.await()
     }
 
-    override fun getAll(forceOnline: Boolean, listener: MainContract.MainModel.OnAdsRequestCompletionListener) = runBlocking<Unit> {
-        var adList: List<Ad> = ArrayList()
+    override fun getById(id: Int): Ad = runBlocking {
+        return@runBlocking async { database.adDao().getById(id) }.await()
+    }
 
-        val job = launch { adList = getAll() }
-        job.join()
+    override fun insert(ad: Ad) = runBlocking {
+        return@runBlocking async { database.adDao().insert(ad) }.await()
+    }
+
+    override fun deleteAd(ad: Ad) = runBlocking {
+        return@runBlocking async { database.adDao().deleteAd(ad) }.await()
+    }
+
+    private fun insertAll(adList: List<Ad>) = runBlocking {
+        for (ad in adList) insert(ad)
+    }
+
+    override fun getAll(forceOnline: Boolean, listener: MainContract.MainModel.OnAdsRequestCompletionListener) = runBlocking {
+        val adList: List<Ad> = getAll()
 
         if (!forceOnline) {
             if (adList.isNotEmpty()) {
-                Log.e("sdoaskdoaskdoosad", "osdksaodksoakdoas")
                 listener.onSucess(adList)
                 return@runBlocking
             }
         }
 
-        requestAds(object : MainContract.MainModel.OnAdsRequestCompletionListener {
-            override fun onSucess(adList: List<Ad>) = runBlocking<Unit> {
-                val job = launch { insertAll(adList) }
-                job.join()
+        requestAdsFromServer(object : MainContract.MainModel.OnAdsRequestCompletionListener {
+            override fun onSucess(adList: List<Ad>) = runBlocking {
+                insertAll(adList)
                 listener.onSucess(adList)
             }
 
@@ -60,7 +61,7 @@ class MainModelImpl : MainContract.MainModel {
         })
     }
 
-    private fun requestAds(listener: MainContract.MainModel.OnAdsRequestCompletionListener) {
+    private fun requestAdsFromServer(listener: MainContract.MainModel.OnAdsRequestCompletionListener) {
         RestClient.getAds().enqueue(object : Callback<AdsApiResult> {
             override fun onResponse(call: Call<AdsApiResult>, response: Response<AdsApiResult>) {
                 if (response.isSuccessful) {
